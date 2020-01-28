0deea4544b30544fbd7769d685ee27338a9676c1
Victor <vaytorh@gmail.com>
2020-01-28 09:21:49 -0300

    Criado todos os métodos

---
+++ Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using Calculadora.Model;
+using System.Collections.Generic;
+
+namespace Calculadora.Services
+{
+    interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
---
+++ Services/Implementation/PersonServiceImpl.cs
@@ -0,0 +1,56 @@
+﻿using Calculadora.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Services.Implementation
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < 8; i++) 
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Victor",
+                LastName = "Souza",
+                Address = "Rua teste 1213",
+                Gender = "Masculino"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
